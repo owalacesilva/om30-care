cd74719761e8beacbc4183755ee5ccff3d5c0e20
Walace Silva <wsilva.emp@gmail.com>
2023-05-25 19:35:08 +0000

    feat: created home page and your specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  # Defines the root path route ("/")
+  root 'home#index'
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 
   resources :towns, only: [:index, :show, :create, :update] do
     resources :residents, only: [:index, :show, :create, :update]
